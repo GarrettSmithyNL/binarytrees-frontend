01cd351f6cba38d195507b05f6a5c3c0a4c7af9a
GarrettSmithyNL <garrett_smith_30@hotmail.com>
2024-12-13 18:55:38 -0330

    bug fixed

--- src/components/Pages/Tree.jsx
+++ src/components/Pages/Tree.jsx
@@ -1,30 +1,61 @@
 import React, {useEffect, useState} from 'react'
 import {NavBar} from "../NavBar";
 
-export const Tree = ({inputId}) => {
+export const Tree = () => {
 
-    const [tree, setTree] = useState([]);
+    const [input, setInput] = useState({});
+    const [tree, setTree] = useState({});
+    const [numbers, setNumbers] = useState("");
 
     useEffect(() => {
-        getTree(inputId);
-    }, [inputId])
+        getInput();
+    },[]);
 
-    const getTree = async (inputId) => {
-        const treeFromServer = await fetchTree(inputId);
-        setTree(treeFromServer);
+    useEffect(() => {
+        if (Object.keys(input).length > 0) {
+            const treeFromInput = input['treeFromInput'];
+            if (treeFromInput && treeFromInput["root"]) {
+                setTree(treeFromInput["root"]);
+            }
+        }
+    }, [input])
+
+    useEffect(() => {
+        if (Object.keys(input).length > 0) {
+            const nums = input['inputs'];
+            if (nums) {
+                const numString = nums.join(',');
+                setNumbers(numString);
+            }
+        }
+    }, [tree]);
+
+
+    const getInput = async () => {
+        const inputFromServer = await fetchInput();
+        setInput(inputFromServer);
     }
 
-    const fetchTree = async (inputId) => {
-        const res = await fetch("http://localhost:8080/tree/1");
+    const fetchInput = async () => {
+        const res = await fetch("http://localhost:8080/input/last");
+        if (res.ok){
+            console.log('got the object')
+        }
+        else
+            console.log('failed to find')
         return await res.json();
     }
 
-
-
     return (
         <div>
-            <NavBar />
-            <pre>{JSON.stringify(tree, null, 4)}</pre>
+            <NavBar/>
+            <h2>Last Input: {numbers}</h2>
+            <div>
+                <h4>JSON Version</h4>
+                <pre>{JSON.stringify(tree, null, 4)}</pre>
+            </div>
+
+
         </div>
     )
 }
